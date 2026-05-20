74ff0a453e0c549829d6ab5c4db30518312d0089
Sara Juliana Faustino Trujillo <sjfaustinot@uqvirtual.edu.co>
2026-05-20 18:47:36 -0500

    asigna responsable por prioridad

--- triage-academy/src/main/java/com/uniquindio/triage_academy/repository/SolicitudRepository.java
+++ triage-academy/src/main/java/com/uniquindio/triage_academy/repository/SolicitudRepository.java
@@ -1,6 +1,8 @@
 package com.uniquindio.triage_academy.repository;
 
 import com.uniquindio.triage_academy.model.entity.Solicitud;
+import com.uniquindio.triage_academy.model.enums.EstadoSolicitud;
+import com.uniquindio.triage_academy.model.enums.Prioridad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -9,4 +11,6 @@ import java.util.UUID;
 
 public interface SolicitudRepository extends JpaRepository<Solicitud, UUID>, JpaSpecificationExecutor<Solicitud> {
     List<Solicitud> findByUsuarioId(UUID usuarioId);
+
+    long countByResponsable_IdAndPrioridadAndEstadoNot(UUID responsableId, Prioridad prioridad, EstadoSolicitud estado);
 }
